073fa9684f6a730825d5f0cae42273435c043ad4
zaherosman <112961714+zaherosman@users.noreply.github.com>
2023-06-16 15:24:24 -0300

    Delete projeto-turma3-app-developer directory

--- projeto-turma3-app-developer/app/src/main/java/com/ibm/teste/data/remote/Favorite/FavoriteObject.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.teste.data.remote.Favorite;
-
-public class FavoriteObject {
-    private Integer userId;
-
-    public FavoriteObject(Integer userId) {
-        this.userId = userId;
-    }
-}
--- projeto-turma3-app-developer/app/src/main/java/com/ibm/teste/data/remote/Trips/TripObject.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.teste.data.remote.Trips;
-
-public class TripObject {
-    private Integer tripId;
-
-    public TripObject(Integer tripId) {
-        this.tripId = tripId;
-    }
-}
